29804db47d98e150f49338fff02009d11cc3e4c7
Rubio <101281624+Rubio2003@users.noreply.github.com>
2025-01-03 18:35:36 +0100

    Arreglado key pressed

--- Imagen/leerCuadricula.m
+++ Imagen/leerCuadricula.m
@@ -1,26 +1,32 @@
-function [cuadricula, tamCuadricula] = leerCuadricula(carpetaNumeros)
+function [cuadricula, tamCuadricula] = leerCuadricula()
     cam = webcam;
-    
+
     % Crea una ventana para mostrar el video en tiempo real.
     h = figure; 
-    set(h, 'KeyPressFcn', @(src, event) assignin('base', 'keyPressed', true));
-    
     keyPressed = false; % Variable para controlar cuando se presiona una tecla
-    
+
+    % Define el callback para manejar la pulsación de teclas
+    set(h, 'KeyPressFcn', @(~, ~) setKeyPressed());
+
     while ishandle(h)
         img = snapshot(cam);
         i = fliplr(img);
-        imshow(i, 'InitialMagnification', 'fit');
+        imshow(i); 
         drawnow;
-    
+
         if keyPressed
+            % Procesa la imagen solo si se ha presionado una tecla
             IPreprocesada = preprocesado(img);
-            cuadricula = imprimirMatriz(IPreprocesada, carpetaNumeros);
+            cuadricula = imprimirMatriz(IPreprocesada);
             tamCuadricula = length(cuadricula);
-            keyPressed = false;
+            keyPressed = false; % Reinicia la variable
         end
     end
-    
+
     close all;
     clear cam;
+
+    function setKeyPressed()
+        keyPressed = true;
+    end
 end
